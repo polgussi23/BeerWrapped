f235b91732f0771f92bf37ebaa2ac39ac2f26ee4
polgussi23 <polgussi23@gmail.com>
2025-02-24 16:22:41 +0100

    Arreglat posicionament imatge spittingBeer al Background

--- lib/components/custom_background.dart
+++ lib/components/custom_background.dart
@@ -17,6 +17,9 @@ class CustomBackground extends StatelessWidget {
     final mediaQuery = MediaQueryData.fromView(View.of(context)); // Manté la mida sense que el teclat afecti
     final screenHeight = mediaQuery.size.height;
     final screenWidth = mediaQuery.size.width;
+
+    final spittingBeerImageHeight = screenHeight * 0.65;
+    final spittingBeerImageWidth = spittingBeerImageHeight / 1.168; // 1.168 és la relació entre amplada i altura de la imatge original!
     
     return Container(
       width: screenWidth,          // Forcem l'amplada del Container a ser tota l'amplada de la pantalla
@@ -52,15 +55,17 @@ class CustomBackground extends StatelessWidget {
           ),
           // Imatge cervesa "spitting_beer" (inferior centre)
           Positioned(
+            
             top: screenHeight * 0.43, // Posició inferior una mica fora de la pantalla per a que sembli tallada
-            //left: -50, // Posició esquerra per centrar-la horitzontalment aproximadament
-            right: -MediaQuery.of(context).size.width*0.3, // Posició dreta per centrar-la horitzontalment aproximadament
+            //left: screenWidth * 0.2, // Posició esquerra per centrar-la horitzontalment aproximadament
+            left: spittingBeerImageLefPosition(screenWidth, spittingBeerImageWidth),
+            //right: -screenWidth*0.3, // Posició dreta per centrar-la horitzontalment aproximadament
             child: Transform.rotate(
               angle: degreesToRadians(-10),
               child: Image.asset(
                 'assets/images/spitting_beer.png', // Ruta a la imatge cervesa gran
-                height: MediaQuery.of(context).size.height * 0.65, // Amplada 120% de la pantalla per a que sobresurti
-                opacity: AlwaysStoppedAnimation(0.3), // Ajusta l'opacitat
+                height: spittingBeerImageHeight, // Amplada 120% de la pantalla per a que sobresurti
+                opacity: const AlwaysStoppedAnimation(0.3), // Ajusta l'opacitat
               ),
             ),
           ),
@@ -76,5 +81,9 @@ class CustomBackground extends StatelessWidget {
 
   double degreesToRadians(double x){
     return x * math.pi / 180;
+  }
+
+  double spittingBeerImageLefPosition(double screenWidth, double imageWidth){
+    return screenWidth - (imageWidth/1.35);
   }
 }
